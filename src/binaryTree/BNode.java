35892f64c6e8bb4c7c5bf9f0827db94196a819f9
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-09-06 11:40:19 -0300

    :page_facing_up: documentação do nó :page_facing_up:

--- src/binaryTree/BNode.java
+++ src/binaryTree/BNode.java
@@ -1,95 +1,114 @@
 package binaryTree;
 
 public class BNode<T> {
+	
     private T data;
     private BNode<T> parent;
     private BNode<T> left;
     private BNode<T> right;
 
-    //Nós em geral
+    /* Construtores */
+    
+    // Construtor para as folhas
     public BNode(T data, BNode<T> parent) {
         this.data = data;
         this.parent = parent;
         right = left = null;
     }
 
-    //Raiz
+    // Construtor para a raiz
     public BNode(T data) {
-        this(data,null);
+        this(data, null);
     }
     
-   
-    public T getData() {
-        return data;
-    }
-
+    /* Setters */
+    
+    // setData(T data) --> atribui um dado de qualquer tipo ao node
     public void setData(T data) {
         this.data = data;
     }
-
-    public BNode<T> getParent() {
-        return parent;
-    }
-
+    
+    // setParent(BNode<T> parent) --> atribui um novo pai para um node
     public void setParent(BNode<T> parent) {
         this.parent = parent;
     }
-
-    public BNode<T> getLeft() {
-        return left;
-    }
-
+    
+    // setLeft(BNode<T> left) --> atribui um novo filho para um node na subárvore à esquerda
     public void setLeft(BNode<T> left) {
         this.left = left;
     }
-
-    public BNode<T> getRight() {
-        return right;
-    }
-
+    
+    // setRight(BNode<T> right) --> atribui um novo filho para um node na subárvore à direita
     public void setRight(BNode<T> right) {
         this.right = right;
     }
     
-    public boolean isRoot(){
+    
+    /* Getters */
+    
+    // getData() --> retorna o dado do node
+    public T getData() {
+        return data;
+    }
+    
+    // getParent() --> retorna o node pai
+    public BNode<T> getParent() {
+        return parent;
+    }
+     
+    // getLeft() --> retorna o filho da subárvore à esquerda 
+    public BNode<T> getLeft() {
+        return left;
+    }
+    
+    // getRight() --> retorna o filho da subárvore à direita 
+    public BNode<T> getRight() {
+        return right;
+    }
+    
+    /* Métodos Adicionais */
+    
+    // isRoot() --> retorna true se um node é uma raiz e false caso contrário
+    public boolean isRoot() {
         return parent == null;
     }
     
-    public boolean isLeaf(){
+    // isRoot() --> retorna true se um node é uma folha e false caso contrário
+    public boolean isLeaf() {
         return left == right;
     }
     
-    public int getDegree(){
+    // getDegree() --> retorna o grau de um node, podendo ser 0, 1 ou 2
+    public int getDegree() {
         int degree = 0;
         if(left != null) ++degree;
         if(right != null) ++degree;
         return degree;
     }
     
+    // getLevel() --> retorna o nível de um node
     public int getLevel() {
         if (isRoot()) return 0;
-        else return parent.getLevel()+1;
+        else return parent.getLevel() + 1;
     }
     
-    public int getHeight(){
+    // getHeight() --> retorna a altura de um node
+    public int getHeight() {
         int h;
         
-        //Folha (Sem Filhos)
-        if(isLeaf()) h = 0; 
-        //Um filho pela direita
-        else if(left == null && right != null) h = 1 + right.getHeight();
-        //Um filho pela esquerda
-        else if(right == null && left != null) h = 1 + left.getHeight();
-        //Dois filhos
-        else h = (left.getHeight() > right.getHeight())
+        if(isLeaf()) h = 0; // Folha (Sem Filhos)
+        else if(left == null && right != null) h = 1 + right.getHeight(); // Um filho pela direita
+        else if(right == null && left != null) h = 1 + left.getHeight(); // Um filho pela esquerda
+        else h = (left.getHeight() > right.getHeight()) // Dois filhos
                 ? 1 + left.getHeight()
                 : 1 + right.getHeight();
         return h;
     }
 
     
-    protected float see(){return Float.NaN;}
-
+    protected float see() { return Float.NaN; }
+    
+    
     @Override
     public String toString() {
         return "Data = " + data 
