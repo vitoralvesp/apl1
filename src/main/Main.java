1c06a657e902f3a3966e7410bd30a117c775ea4d
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-09-06 14:36:36 -0300

    working...

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,43 +2,110 @@ package main;
 import binaryTree.*;
 import operations.*;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        while(true){
-            try{
-                System.out.println("###############MENU###############");
-                System.out.print("[1] Entrada da expressão aritmética na notação infixa.\n" +
-                                 "[2] Criação da árvore binária de expressão aritmética.\n" +
-                                 "[3] Exibição da árvore binária de expressão aritmética.\n" +
-                                 "[4] Cálculo da expressão (realizando o percurso da árvore).\n" +
-                                 "[5] Sair.\nOpção = ");
-                int opt = inp.nextInt();
-                if(opt == 5) break;
-                if(opt < 1 || opt > 5) {
-                    System.out.println("Digite um número válido!\n");
+    
+	public static void main(String[] args) {
+        
+		Scanner scanner = new Scanner(System.in);
+		int option;
+		String infixNotation;
+		String[] infixNotationArr;
+		int infixNotationLength;
+		BTree tree;
+		BNode node;
+		
+		while(true) {
+			
+			infixNotation = "";
+			infixNotationLength = 0;
+			infixNotationArr = new String[] {};
+			node = new BNode("+");
+            
+			try {
+				
+				System.out.println("--------------------------------------------------------");
+                System.out.println("MENU:");
+                System.out.print("[1] Entrada da expressao aritmetica na notaçao infixa.\n" +
+                                 "[2] Criacao da arvore binaria de expressao aritmetica.\n" +
+                                 "[3] Exibicao da arvore binaria de expressao aritmetica.\n" +
+                                 "[4] Calculo da expressao (realizando o percurso da arvore).\n" +
+                                 "[5] Sair.\n\nEscolha uma opcao: ");
+                
+                option = scanner.nextInt();
+
+                if (option == 5) {
+                	scanner.close();
+                	break;
+                }
+                
+                if(option < 1 || option > 5) {
+                	System.out.println("--------------------------------------------------------\n");
+                    System.out.println("\nOPCAO INVALIDA!!! Digite um numero valido de 1 a 5...\n");
                     continue;
                 }
-                System.out.println("##################################");
+                System.out.println("--------------------------------------------------------\n");
                 
-                switch(opt){
+                switch(option) {
+                
                     case 1 -> {
-                        System.out.println("teste");
+                        
+                    	scanner.nextLine();
+                    	
+                    	System.out.print("Digite uma expressao aritmetica na forma infixa: ");
+                    	infixNotation = scanner.nextLine().replaceAll("\\s", "");
+                    	infixNotationArr = infixNotation.split(infixNotation);
+                    	break;
+                    	
                     }
+                    
                     case 2 -> {
                         
+                    	// infixNotationLength = infixNotation.length();
+                    	for (String character : infixNotationArr) {
+                    		
+                    		if (character == "+" || 
+                    			character == "-" ||
+                    			character == "*" ||
+                    			character == "/" )
+                    			node.setData(character);
+                    		else {
+                    			
+                    			                  			
+                    			
+                    		}
+                    		
+                    		                   		
+                    		
+                    	}
+                    		
+                    	                   	
+                    	break;
+                    	
                     }
+                    
                     case 3 -> {
                         
+                    	System.out.println("Case 3\n");
+                    	break;
+                    	
                     }
+                    
                     case 4 -> {
                         
+                    	System.out.println("Case 4\n");
+                    	break;
+                    
                     }
                 }
-            }catch(NumberFormatException e){
-                System.out.println("Digite um número válido!\n");
+            
+			} catch(InputMismatchException e) {
+				System.out.println("--------------------------------------------------------\n");
+				System.out.println("\nENTRADA INVALIDA!!! Tente novamente apenas com números inteiros de 1 a 5...\n");
+				scanner.nextLine();
+                
             }
             
         }
