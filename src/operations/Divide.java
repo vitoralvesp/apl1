1c227cf3628c905f457181ed7c0b75ca207d82fa
Vitor Alves Pereira <68354632+vitoralvesp@users.noreply.github.com>
2024-09-12 20:52:14 -0300

    :heavy_check_mark: documentação finalizada em Divide.java

--- src/operations/Divide.java
+++ src/operations/Divide.java
@@ -1,5 +1,3 @@
-package APL1.src.operations;
-
 /**
  *
  * @author Lucas Pires de Camargo Sarai - 10418013
@@ -14,17 +12,22 @@ package APL1.src.operations;
  *  Árvores - fundamentos
  */
 
+package APL1.src.operations;
 import APL1.src.binaryTree.*;
 
 public class Divide extends Operator {
 
+    // Divide(dado, pai) --> acessa os atributos data e parent do node para realizar a operação de divisão
     public Divide(char data, BNode parent) {
         super(data, parent);
     }
 
+    // Divide(dado) --> acessa o atributo data do node para realizar a operação de divisão 
     public Divide(char data) {
         super(data);
     }
+
+    // see() --> realiza a operação de divisão para os filhos (operandos) da esquerda e direita do node (é considerado 0 se um deles for nulo)
     @Override
     protected float see() {
         Operand leftOp = (Operand)this.getLeft();
